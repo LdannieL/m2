2c693c5fc9ce211611c7d1bd4cd065285caa9fd6
Danijela Lapcevic <dannie@eunet.rs>
2017-09-13 01:22:32 +0200

    delete

---
+++ app/etc/env.php
@@ -0,0 +1,63 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_nde680',
+  ),
+  'crypt' => 
+  array (
+    'key' => '137a1c43263f10c0561f32f19c59c41c',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2',
+        'username' => 'root',
+        'password' => 'dannie',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'full_page' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+    'compiled_config' => 0,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sun, 15 Jan 2017 13:30:56 +0000',
+  ),
+);
